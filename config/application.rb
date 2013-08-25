158802b01e3ddcba925c3810f994a22d63ccd0e2
Himesh Ladva <himeshladva@gmail.com>
2013-08-25 17:23:58 +0100

    add Century Gothic font

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module PortfolioRails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Enable below line to use 'font-url in scss files'
+    # config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
