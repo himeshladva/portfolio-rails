5098f022fd1e1a27eae2725d8a7c82b3ef09dcfe
Himesh Ladva <himeshladva@gmail.com>
2013-08-27 16:56:23 +0100

    add page fade transitions

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,3 +1,24 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+  var Initialize = {
+    init: function () {
+      // PageFade.init();
+    }
+  }
 
+  var PageFade = {
+    init: function () {
+      $(".content-container").css("display", "none");
+      $(".content-container").fadeIn(200);
+
+      $('.nav a').click(function (e) {
+        var redirect = $(this).attr('href');
+        e.preventDefault();
+        $('.content-container').fadeOut(200, function (){
+          document.location.href = redirect;
+        });
+      });
+    }
+  };
+
+  Initialize.init();
 });
