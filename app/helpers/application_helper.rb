6bdd7dd8b18aca19bd8d07aece8b73d545927561
Himesh Ladva <himeshladva@gmail.com>
2013-08-27 01:54:23 +0100

    add css file for each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
   def selected?(path)
     current_page?(path) ? 'selected' : ''
   end
+
+  def current_page
+    case request.path
+      when '/'
+        return 'home'
+      when '/about'
+        return 'about'
+      when '/portfolio'
+        return 'portfolio'
+      when '/contact'
+        return 'contact'
+    end
+  end
 end
