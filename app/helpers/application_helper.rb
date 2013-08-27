80514cdef1d126fd8375af5099b4a58b5cafe3a8
Himesh Ladva <himeshladva@gmail.com>
2013-08-27 17:32:54 +0100

    add option for custom page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,14 @@ module ApplicationHelper
     render 'shared/meta'
   end
 
+  def page_meta(options)
+    @page_meta_options = options
+  end
+
+  def page_title
+    @page_meta_options[:title]
+  end
+
   def selected?(path)
     current_page?(path) ? 'selected' : ''
   end
