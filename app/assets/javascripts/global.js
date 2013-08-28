7b908fc7c68ab84331a27d05259e6409baacc229
Himesh Ladva <himeshladva@gmail.com>
2013-08-28 22:21:07 +0100

    set height to current window height

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,21 +2,37 @@ $(document).ready(function () {
   var Initialize = {
     init: function () {
       // PageFade.init();
+      SetHeight.init();
     }
   }
 
   var PageFade = {
     init: function () {
-      $(".content-container").css("display", "none");
-      $(".content-container").fadeIn(200);
+      $(".content-wrapper").css("display", "none");
+      $(".content-wrapper").fadeIn(200);
 
       $('.nav a').click(function (e) {
         var redirect = $(this).attr('href');
         e.preventDefault();
-        $('.content-container').fadeOut(200, function (){
+        $('.content-wrapper').fadeOut(200, function (){
           document.location.href = redirect;
         });
       });
+    }
+  };
+
+  var SetHeight = {
+    init: function () {
+      this.resize();
+      var self = this;
+      $(window).resize(function() {
+        self.resize();
+      });
+    },
+    resize: function () {
+      var height = $(window).height() - 130;
+      $('.content').height(height);
+      $('.sidebar').height(height);
     }
   };
 
