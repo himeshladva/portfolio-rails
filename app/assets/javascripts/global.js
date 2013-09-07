8a1fe116b1df5df197c9d0189e1a24ff4836ffda
Himesh Ladva <himeshladva@gmail.com>
2013-09-07 22:41:50 +0100

    enabled fixed width sidebar

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -61,17 +61,17 @@ $(document).ready(function () {
 
     hideSidebar: function () {
       $('.sidebar-container').stop().animate({'margin-right': '-500px'}, 500);
-      $('.content-container').stop().animate({'width': '100%'}, 500);
+      $('.content-container').stop().animate({'padding-right': '0'}, 500);
       $('.content').stop().animate({'margin': '0 20px'}, 500);
-      $('.sidebar-open').stop().fadeIn(500);
+      $('.sidebar-open').stop().fadeIn(800);
       $('.sidebar-close').stop().fadeOut(500);
     },
 
     showSidebar: function () {
       $('.sidebar-container').stop().animate({'margin-right': '0px'}, 500);
-      $('.content-container').stop().animate({'width': '80%'}, 500);
+      $('.content-container').stop().animate({'padding-right': '275px'}, 500);
       $('.content').stop().animate({'margin': '0 10px 0 20px'}, 500);
-      $('.sidebar-open').stop().fadeOut(500);
+      $('.sidebar-open').stop().fadeOut(300);
       $('.sidebar-close').stop().fadeIn(500);
     }
   };
