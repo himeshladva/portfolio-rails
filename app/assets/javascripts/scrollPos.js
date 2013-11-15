a92b311cdd460f15efd3797cb28d7fa0790ff8c2
Himesh Ladva <himeshladva@gmail.com>
2013-11-15 23:26:44 +0000

    refactor scrollPos.js to match menuClass.js

--- app/assets/javascripts/scrollPos.js
+++ app/assets/javascripts/scrollPos.js
@@ -1,27 +1,26 @@
 var scrollPos = {
   init: function () {
-    // SHADOW
-    this.resize([$('header.masthead')], 'shadow', 10);
+    var self = this;
+    $(window).scroll(function () {
+      // SHADOW
+      self.resize([$('header.masthead')], 'shadow', 10);
 
-    // SMALL HEADER
-    this.resize([$('header.masthead'), $('header .logo')], 'small', 300);
+      // SMALL HEADER
+      self.resize([$('header.masthead'), $('header .logo')], 'small', 300);
+    });
   },
 
   resize: function (el, newClass, desiredPos) {
-    var self = this;
-
-    $(window).scroll(function () {
-      if (self.isMobile()) {
-        for (var i in el) {
-          var currentPos = $(this).scrollTop();
-          if (currentPos > desiredPos) {
-            el[i].addClass(newClass);
-          } else {
-            el[i].removeClass(newClass);
-          }
+    if (this.isMobile()) {
+      for (var i in el) {
+        var currentPos = $(window).scrollTop();
+        if (currentPos > desiredPos) {
+          el[i].addClass(newClass);
+        } else {
+          el[i].removeClass(newClass);
         }
       }
-    });
+    }
   },
 
   isMobile: function () {
