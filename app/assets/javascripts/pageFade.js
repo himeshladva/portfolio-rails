addfa0587707000d3745898d2b5f3299b5ccab79
Himesh Ladva <himeshladva@gmail.com>
2013-11-29 20:28:12 +0000

    refactore pageFade.js

--- app/assets/javascripts/pageFade.js
+++ app/assets/javascripts/pageFade.js
@@ -1,14 +1,22 @@
 var pageFade = {
   init: function () {
-    $(".content-container").hide();
-    $(".content-container").fadeIn(200);
+    this.fadeShow();
 
     $('.nav a').click(function (e) {
-      var redirect = $(this).attr('href');
-      e.preventDefault();
-      $('.content-container').fadeOut(200, function (){
-        document.location.href = redirect;
-      });
+      this.fadeRedirect(e);
+    });
+  },
+
+  fadeShow: function () {
+    $(".content-container").hide();
+    $(".content-container").fadeIn(200);
+  },
+
+  fadeRedirect: function (e) {
+    var redirect = $(this).attr('href');
+    e.preventDefault();
+    $('.content-container').fadeOut(200, function (){
+      document.location.href = redirect;
     });
   }
 };
