4e51ca389952832d2711370aacc4a590868e1cbb
Himesh Ladva <himeshladva@gmail.com>
2013-12-06 22:43:29 +0000

    stop using controller to navigate sections

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-
-  def about
-    redirect_to "#{root_url}#about"
-  end
-
-  def portfolio
-    redirect_to "#{root_url}#portfolio"
-  end
-
-  def contact
-    redirect_to "#{root_url}#contact"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 PortfolioRails::Application.routes.draw do
   root to: "home#index"
 
-  get 'about', to: "home#about", as: :page_about
-  get 'portfolio', to: "home#portfolio", as: :page_portfolio
-  get 'contact', to: "home#contact", as: :page_contact
+  # get 'about', to: "home#about", as: :page_about
+  # get 'portfolio', to: "home#portfolio", as: :page_portfolio
+  # get 'contact', to: "home#contact", as: :page_contact
 end
