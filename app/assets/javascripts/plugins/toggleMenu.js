1681f9f6cbbb42f8e46a928d2e22bd01216a2468
Himesh Ladva <himeshladva@gmail.com>
2013-12-22 17:54:52 +0000

    add functionality to change menu icon

--- app/assets/javascripts/plugins/toggleMenu.js
+++ app/assets/javascripts/plugins/toggleMenu.js
@@ -1,13 +1,37 @@
 var toggleMenu = {
   init: function () {
-    $("div.menu-pull").click(function () {
-      event.preventDefault();
-      $(".nav-container").slideToggle();
+    var self = this;
+
+    $("div.closed").click(function () {
+      $(".nav-container").fadeToggle(100, self.assertClasses($(this)));
     });
 
     $("nav.small a").click(function () {
-      console.log('link');
       $(".nav-container").hide();
     });
+  },
+
+  assertClasses: function ($el) {
+    var classList = $el[0].classList;
+
+    if (classList[1] == 'closed') {
+      this.menuActive($el);
+      return
+    }
+
+    if (classList[1] == 'active') {
+      this.menuClosed($el);
+      return
+    }
+  },
+
+  menuActive: function ($el) {
+    $el.removeClass('closed');
+    $el.addClass('active');
+  },
+
+  menuClosed: function ($el) {
+    $el.removeClass('active');
+    $el.addClass('closed');
   }
 };
