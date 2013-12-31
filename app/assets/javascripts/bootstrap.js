d4bc48f8877f07eb32887c37aded75a06e991bb3
Himesh Ladva <himeshladva@gmail.com>
2013-12-31 20:30:03 +0000

    move carousel js to separate file

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -8,27 +8,7 @@ $(document).ready(function () {
       scrollPos.init();
       smoothScroll.init();
       toggleMenu.init();
-
-      $(function() {
-        $(".rslides").responsiveSlides({
-          auto: true,
-          speed: 100,
-          timeout: 4000,
-          pager: true,
-          nav: true,
-          random: false,
-          pause: false,
-          pauseControls: true,
-          prevText: "Previous",
-          nextText: "Next",
-          maxwidth: "",
-          navContainer: "",
-          manualControls: "",
-          namespace: "transparent-btns",
-          before: function () {},
-          after: function () {}
-        });
-      });
+      heroCarousel.init();
     }
   }
 
---
+++ app/assets/javascripts/plugins/heroCarousel.js
@@ -0,0 +1,24 @@
+var heroCarousel = {
+  init: function () {
+    $(".rslides").responsiveSlides(this.options);
+  },
+
+  options: {
+    auto: true,
+    speed: 100,
+    timeout: 4000,
+    pager: true,
+    nav: true,
+    random: false,
+    pause: false,
+    pauseControls: true,
+    prevText: "Previous",
+    nextText: "Next",
+    maxwidth: "",
+    navContainer: "",
+    manualControls: "",
+    namespace: "transparent-btns",
+    before: function () {},
+    after: function () {}
+  }
+}
